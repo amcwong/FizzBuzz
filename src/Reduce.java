335380b5691e31dc7a74a6a2f8b9a0e28b363376
amcwong <andrew.wong8@icloud.com>
2023-09-11 18:57:22 -0400

    initial reduce

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+    public static void main(String[] args) {
+        int n = 100;
+        int step = 1;
+
+        while (n > 0){
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n -= 1;
+            }
+            step++;
+    //        System.out.println(step);
+            System.out.println(n);
+        }
+
+    }
 }
